39bec0ee76d1c0a5214084eac3131cbd1b37184f
Rob Rossmiller <rrossmiller3@gmail.com>
2023-05-02 13:55:11 -0400

    update writer to pid.txt

--- run_app.sh
+++ run_app.sh
@@ -1,7 +1,9 @@
 cd ui
+echo "UI"  > ../pid.txt
 nohup yarn dev --host 0.0.0.0 &
-echo $! > ../pid.txt
+echo $! >> ../pid.txt
 
 cd ../api
+echo "API"  >> ../pid.txt
 nohup ./tracker-api &
 echo $! >> ../pid.txt
